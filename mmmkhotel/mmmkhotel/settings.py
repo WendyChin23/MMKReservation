b2bb854658b49d9633dbb4cf0b897e55f39200af
WendyChin23 <kristinevelasquez23@gmail.com>
2022-02-23 11:49:38 +0800

    prefinal design

--- mmmkhotel/mmmkhotel/settings.py
+++ mmmkhotel/mmmkhotel/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    "GitHub\MMKReservation\mmmkhotel\mmkreservation\static"
+    "mmkreservation\static"
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
